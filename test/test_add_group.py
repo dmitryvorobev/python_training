0367326c964942ffdc8560ecbdf13b6504b193ea
dmitryvorobev <factanonverba@yandex.ru>
2016-03-20 10:14:29 +0300

    extracted class for working with groups

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,11 +14,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="sdfgh", header="lkjhg", footer="poiuy"))
+    app.group.create(Group(name="sdfgh", header="lkjhg", footer="poiuy"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
