59ab18ae15f8c2edd595ca109b7766df8c30cb85
dmitryvorobev <factanonverba@yandex.ru>
2016-03-23 23:34:09 +0300

    Задание №6
    Общая фикстура, метод удаления группы

--- fixture/group.py
+++ fixture/group.py
@@ -31,3 +31,14 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #delete group
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+
