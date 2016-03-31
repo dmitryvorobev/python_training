59596325ed20d6b2359733e1fdbe660663c80bd6
dmitryvorobev <factanonverba@yandex.ru>
2016-04-01 00:34:22 +0300

    Задание №10
    Задание №10: оптимизировать переходы между страницами

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,28 +8,18 @@ class ContactHelper:
     def add_contact_fill_form(self, Person):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Person.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Person.lastname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(Person.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(Person.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(Person.home_phone_num)
+
+        self.change_field("firstname",Person.firstname)
+        self.change_field("lastname",Person.lastname)
+        self.change_field("company",Person.company)
+        self.change_field("address",Person.address)
+        self.change_field("home",Person.home_phone_num)
+        self.change_field("byear",Person.year)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(Person.year)
+
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def delete_first_contact(self):
@@ -45,33 +35,41 @@ class ContactHelper:
         #click edit by first
         wd.find_element_by_xpath("//tr[2]/td/a/img[@title='Edit']").click()
 
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Person.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Person.lastname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(Person.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(Person.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(Person.home_phone_num)
+        self.change_field("firstname",Person.firstname)
+        self.change_field("lastname",Person.lastname)
+        self.change_field("company",Person.company)
+        self.change_field("address",Person.address)
+        self.change_field("home",Person.home_phone_num)
+        self.change_field("byear",Person.year)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(Person.year)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         wd.find_element_by_xpath("//form/input[@value='Update'][1]").click()
 
 
     def count(self):
         wd = self.app.wd
         return len(wd.find_elements_by_xpath("//td/input[@name='selected[]']"))
+
+    def change_field(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_group_form(self, Person):
+        wd = self.app.wd
+        self.change_field("firstname",Person.firstname)
+        self.change_field("lastname",Person.lastname)
+        self.change_field("company",Person.company)
+        self.change_field("address",Person.address)
+        self.change_field("home",Person.home_phone_num)
+        self.change_field("byear",Person.year)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[3]").click()
