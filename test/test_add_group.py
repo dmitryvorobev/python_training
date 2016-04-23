f81d09026d95e549919641c6d5f2f208102960a0
dmitryvorobev <factanonverba@yandex.ru>
2016-04-23 19:24:48 +0300

    Задание №19
    Задание №19: Реализовать загрузку тестовых данных из файлов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,8 +5,8 @@ from data.groups import testdata
 
 
 
-def test_add_group(app,data_groups):
-    group = data_groups
+def test_add_group(app,json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
