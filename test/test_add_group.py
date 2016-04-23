6739804d00ab2c8d23a0018d5451bf0ecacd45a5
dmitryvorobev <factanonverba@yandex.ru>
2016-04-23 18:55:13 +0300

    до 18

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-from data.add_group import constant as test_data
+from data.groups import testdata
 
 
 
-@pytest.mark.parametrize("group",test_data, ids=[repr(x) for x in test_data])
-def test_add_group(app,group):
+def test_add_group(app,data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
