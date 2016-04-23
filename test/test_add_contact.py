25d1b6509f006aa636d28ae9376a5751a335a9d2
dmitryvorobev <factanonverba@yandex.ru>
2016-04-23 19:42:00 +0300

    Задание №19
    Задание №19: Реализовать загрузку тестовых данных из файлов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,8 +5,8 @@ import random
 import string
 
 
-@pytest.mark.parametrize("contact",test_data, ids=[repr(x) for x in test_data])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add_contact_fill_form(contact)
     assert len(old_contacts) + 1 == app.contact.count()
